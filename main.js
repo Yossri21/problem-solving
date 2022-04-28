17daeacb5b0d506a7823287a06291e6ab2ad2e17
Yossri21 <yosriasfour@gmail.com>
2022-04-29 00:47:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const romanMap = {
+  1: 'I',
+  2: 'II',
+  3: 'III',
+  4: 'IV',
+  5: 'V',
+  6: 'VI',
+  7: 'VII',
+  8: 'VIII',
+  9: 'IX',
+  10: 'X',
+};
+
+export const intToRoman = function (num) {
+  return romanMap[num.toString()];
+};
---
+++ main.test.js
@@ -0,0 +1,16 @@
+//const func = require('./main');
+ 
+
+import { intToRoman } from './main.js'
+
+test('Outputs III', () => {
+  expect(intToRoman(3)).toBe('III');
+});
+
+test('Outputs IV', () => {
+  expect(intToRoman(5)).toBe('IV');
+});
+
+test('Outputs IX', () => {
+  expect(intToRoman(9)).toBe('IX');
+});
